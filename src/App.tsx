c9a0a9e33f94ef6085574fb5cc7e1ca6278263b8
Nick Bansal <nick.bansal@autotrader.co.uk>
2024-05-17 13:19:22 +0100

    added in react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Movies } from "components/movies/movies";
 import { Products } from "components/products/products";
@@ -9,30 +10,21 @@ import { Header } from "./components/header/header";
 import "./App.css";
 
 function App() {
-  // const [testData, setTestData] = useState();
-
-  // const testAPI = async () => {
-  //   const dataFromAPI = await fetch("https://dummyapi.online/api/movies");
-  //   const dataInJson = await dataFromAPI.json();
-  //   setTestData(dataInJson);
-  // };
-
-  // useEffect(() => {
-  //   testAPI();
-  // }, []);
-
-  // console.log(testData);
-
   return (
-    <div className="App">
-      <Header />
-      <h1>The followings are products</h1>
-      <Products />
-      <h1>The followings are movies</h1>
-      <Movies />
-      <h1>The followings are Users</h1>
-      <Users />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/products" Component={Products} />
+        </Routes>
+        <Routes>
+          <Route path="/movies" Component={Movies} />
+        </Routes>
+        <Routes>
+          <Route path="/users" Component={Users} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
